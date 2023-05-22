5be5ace767956f3256e183c4629edbf6277f9f7f
DanielGrenehed <daniel.grenehed97@gmail.com>
2023-05-23 00:44:15 +0200

    image loading

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 const WebSocket = require('ws');
 const ws_port = 8002;
 const wss = new WebSocket.Server({port: ws_port});
 clients = [];
+let images = {"/test.webp":fs.readFileSync('./res/test.webp'),
+	"/klimio":fs.readFileSync('./res/klimio.png')
+};
 
 let play = [[{text:"LiA på Klimio", x: 0.1, y: 0.2, size: 10, fade:0.01},
-{text:"Daniel Grenehed - Iot21", x:0.1, y: 0.27, size: 6, fade:0.007}],
+{text:"Daniel Grenehed - Iot21", x:0.1, y: 0.27, size: 6, fade:0.007},
+{img:"/klimio", x: 0.8, y: 0.19, scale: 1.5, fade: 0.009}],
 "clear"];
+
+function isImage(r) {
+	if (images.hasOwnProperty(r)) return true;
+	return false;
+}
+function sendImage(r, res) {
+	if (!isImage(r)) return;
+	res.writeHead(200, {'content-type':'image/gif'});
+	res.end(images[r], 'binary');
+}
 
 function handleClientMSG(client, msg) {
 	console.log(msg);
@@ -32,8 +47,19 @@ const port = 8001;
 const html = fs.readFileSync("index.html", 'utf8');
 
 http.createServer((req, res) => {
-	res.writeHead(200, {'content-type':'text/html'});
-	res.end(html);
+	let r = url.parse(req.url, true).pathname;
+	console.log(r);
+	if (r === "/favicon.ico") {
+		res.writeHead(404, {'content-type':'text/html'});
+		res.end();
+		return;
+	}
+	else if (isImage(r)) {
+		sendImage(r, res);
+	} else {
+		res.writeHead(200, {'content-type':'text/html'});
+		res.end(html);
+	}
 }).listen(port);
 
 console.log("HTTP server started (port: %i)", port);
