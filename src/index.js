d9a39bac52b9bd24a13707647b4c69c1009ad286
DanielGrenehed <daniel.grenehed97@gmail.com>
2023-05-22 19:42:09 +0200

    fullscreen btn

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ const port = 8001;
 
 http.createServer((req, res) => {
 	res.writeHead(200, {'content-type':'text/html'});
-	res.end(`<title>Hello world</title><div id="full">hello<div><script>
-		var e = document.getElementById("full");
-		e.requestFullscreen();
-		e.webkitRequestFullscreen();
-		e.msRequestFullscreen();
-		</script>`);
+	res.end(`<title>Hello world</title><div id="full">hello<div>
+		<button id="fullsc" onclick="fullscreen()">Fullscreen</button>
+	<script>
+		function fullscreen() {
+			let e = document.getElementById("full");	
+			console.log(e.requestFullscreen());
+		}
+	</script>`);
 }).listen(port);
 
 console.log("HTTP server started (port: %i)", port);
